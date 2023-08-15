26b812a6ebc84fffd132e487ec21231fc8374962
Bukola <bukolam.ajayi@gmail.com>
2023-08-15 01:26:02 +0100

    start

--- Desktop/Original/org.py
+++ Desktop/Original/org.py
@@ -1,4 +1,33 @@
-a = 1
-print(a)
+# a = 1
+# print(a)
 
 
+# # count = 0
+# # while count < 9:
+# #     print('the count;', count )
+# #     count += 1
+# print('goodbye')
+
+
+# for x in 'geeksforgeeks':
+#             if x!= 'k' and x!= 's':
+#                 print(x)
+
+# def tripflow(destination):
+#         print('it is good')
+
+# tripflow('kampala')
+
+
+# def cal_taxi_price(price,rate, discount = 5):
+#     print(price * rate * discount)
+
+# cal_taxi_price(2,3)
+
+
+def cal_taxi_price(us_dollars,exchange_rate):
+    return us_dollars * exchange_rate
+
+a = cal_taxi_price(4, 5)
+
+print("this is the;", a)
