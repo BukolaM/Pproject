d80026922ef05af39bc5597da7fa1b6c2873d35b
Bukola <bukolam.ajayi@gmail.com>
2023-08-15 02:30:09 +0100

    ok

--- Desktop/Original/org.py
+++ Desktop/Original/org.py
@@ -28,12 +28,29 @@
 def cal_taxi_price(us_dollars,exchange_rate):
     return us_dollars * exchange_rate
 
-a = cal_taxi_price(4, 5)
+# a = cal_taxi_price(4, 5)
 
-print("this is the;", a)
-b = 3
-print(b)
-d = 4
-print(d)
+# print("this is the;", a)
+# b = 3
+# print(b)
+# d = 4
+# print(d)
+
+by = {
+    "a" : 1,
+    "b": 2,
+    "c": 3
+}
+
+print(by["a"])
+
+
+def highest_num(a):
+    return max(a)
+
+max_num = highest_num([1,5,3])
+print(max_num)
+
+
 
 
