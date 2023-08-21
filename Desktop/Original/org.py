30ccf857674eeae0da0b4d78c50771ec27f2db73
Bukola <bukolam.ajayi@gmail.com>
2023-08-21 21:56:53 +0100

    python manipulation

--- Desktop/Original/org.py
+++ Desktop/Original/org.py
@@ -1,80 +1,75 @@
-# a = 1
-# print(a)
 
+1. 
 
-# # count = 0
-# # while count < 9:
-# #     print('the count;', count )
-# #     count += 1
+# count = 0
+# while count < 9:
+# print('the count;', count )
+# count += 1
 # print('goodbye')
 
 
-# for x in 'geeksforgeeks':
-#             if x!= 'k' and x!= 's':
-#                 print(x)
-
-# def tripflow(destination):
-#         print('it is good')
-
-# tripflow('kampala')
-
-
-# def cal_taxi_price(price,rate, discount = 5):
-#     print(price * rate * discount)
-
-# cal_taxi_price(2,3)
-
-
-def cal_taxi_price(us_dollars,exchange_rate):
-    return us_dollars * exchange_rate
-
-# a = cal_taxi_price(4, 5)
-
-# print("this is the;", a)
-# b = 3
-# print(b)
-# d = 4
-# print(d)
-
-# by = {
-#     "a" : 1,
-#     "b": 2,
-#     "c": 3
-# }
-
-# print(by["a"])
-
-
-# def highest_num(a):
-#     return max(a)
-
-# max_num = highest_num([1,5,3])
-# print(max_num)
-
-
 # -------------------------------------------------
-def squared_numbers(numbers):
-    dicti = {}
-    count = 0
+2. 
+"""
+Write a Python program to get a string made of the first 2
+and last 2 characters of a given string. If the string length is less than 2, return the empty string instead."""
+# Sample String : 'w3resource'
 
-    for number in numbers:
-        key = number
-        value = number * number
-        count = count + 1
-        dicti[key] = value
-    return dicti
-
-
-result = squared_numbers(range(5,16))
-print(result)
-
-
-
-# ------------------------------------------------
-# import numpy as np
-# tuple1 = ((1,2,3),(4,5,6))
-# array1 = np.array(tuple1)
-# print(array1)
+# def string_test(items):
+#         if len(items) > 2:
+#             return items[:2] + items[-2:]
+#         else:
+#             return ""
+        
+# result = string_test('w3resource')
+# print(result)
 
 
+# ---------------------------------------------
+3. 
+"""
+Write a Python program to add 'ing' at the end of a given string (length should be at least 3). If the given string 
+already ends with 'ing', add 'ly' instead. If the string length of the given string is less than 3, leave it unchanged"""
+
+# def string_check(item):
+#     if item[-3:] == "ing":
+#         return item + "ly"
+#     elif len(item) > 3:
+#         return item + "ing"
+#     else:
+#         return item
+
+# result = string_check('w3resource')
+# print(result)
+
+
+
+# -----------------------------------------------------------
+4. 
+"""
+Write a Python function that takes a list of words and return
+ the longest word and the length of the longest one"""
+
+def longest_length(items):
+    length = 0
+    name = ''
+    for item in items:
+        result = len(item)
+        if result > length:
+             length = result
+             name = item
+    return { 'longest_word' : name,
+            'longest length' : length
+
+    }
+
+results = longest_length(['book','sample', 'expect', 'scenerios', 'juggle'])
+print(results)
+
+# -------------------------------------------------------------
+5.  
+"""
+Write a Python program to convert a given string into a list of words.
+['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog.']
+['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog.']"""
 
